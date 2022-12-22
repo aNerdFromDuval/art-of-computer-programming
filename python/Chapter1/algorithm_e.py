991980d34f8de010a4f75a4975863699245a8829
Terrence Williams <terrence@dubtroop.com>
2022-12-21 20:56:00 -0500

    Implementing algorithm_e.py

---
+++ python/Chapter1/algorithm_e.py
@@ -0,0 +1,39 @@
+# Euclid's Algorithm
+# Compute the greatest common divisor of two integers
+#
+# Input: Two integers
+# Output: The greatest common divisor of the two integers
+
+"""
+Euclid's Algorithm
+Compute the greatest common divisor of two integers
+
+Algorithm:
+ - E0: [Ensure m >= n] If m < n, exchange m and n
+    - E1: [Find the Remainder] Divide m by n and let r be the remainder. (We have m = nq + r, where q is the quotient and r is the remainder)
+    - E2: [Is it zero?] If r = 0, the algorithm terminates; n is the answer
+    - E3: [Reduce] Set m <- n, n <- r, and go back to step E1
+
+
+Input: Two integers
+Output: The greatest common divisor of the two integers
+"""
+
+
+def algorithm_e(m, n):
+    if m < n:
+        m, n = n, m
+    while True:
+        r = m % n
+        if r == 0:
+            return n
+        m, n = n, r
+
+
+def algorithm_e_recursive(m, n):
+    if m < n:
+        m, n = n, m
+    r = m % n
+    if r == 0:
+        return n
+    return algorithm_e_recursive(n, r)
